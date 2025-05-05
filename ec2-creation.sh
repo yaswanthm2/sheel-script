98578cf554ce5f9aff1bebc59a9b59bcc34df212
Yaswanth <yaswanth@>
2025-05-05 14:58:11 +0530

    ec2-creation file created

---
+++ ec2-creation.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+R=\e[31m    #it will give text red color 
+G=\e[32m    #it will give text green color 
+N=\e[0m     #it will give normal color to text 
+
+DATE=$(date +%F)
+SCRIPTFILE-NAME=$0
+LOG-FILE=/tmp/$SCRIPTFILE-NAME-$DATE.log
+
+INSATNCES_NAMES=("mongodb" "cart" "app" "mysql")
+
+for i in ${INSATNCES_NAMES[@]}
+   do 
+     echo -e  " $G names of instances : $i $N "
+    
+    done
+    
---
+++ variables/01-variable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+echo "Hello , Hi this is yaswanth"
+
+
+echo "ramesh : hi suresh , how are you "
+echo "suresh : hi ramesh , i am fine and you"
+echo "ramesh : i am fine suresh , where are uou going"
+echo "suresh : i am going to bangalore ramesh"
