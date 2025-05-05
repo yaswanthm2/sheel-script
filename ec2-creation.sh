53faf0edfeb3496d66a80f4f8d71375ed02502a5
Yaswanth <yaswanth@>
2025-05-05 15:25:53 +0530

    ec2-server script

--- ec2-creation.sh
+++ ec2-creation.sh
@@ -5,8 +5,8 @@ G=\e[32m    #it will give text green color
 N=\e[0m     #it will give normal color to text 
 
 DATE=$(date +%F)
-SCRIPTFILE-NAME=$0
-LOG-FILE=/tmp/$SCRIPTFILE-NAME-$DATE.log
+SCRIPTFILE_NAME=$0
+LOG-FILE=/tmp/$SCRIPTFILE_NAME-$DATE.log
 
 INSATNCES_NAMES=("mongodb" "cart" "app" "mysql")
 
@@ -15,4 +15,4 @@ for i in ${INSATNCES_NAMES[@]}
      echo -e  " $G names of instances : $i $N "
     
     done
-    
+
