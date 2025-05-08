9f724e2e8f5e5334efbf73d244ce2e9770be796a
Yaswanth <yaswanth@>
2025-05-08 12:28:38 +0530

    adding server creation script

--- variables/create-servers.sh
+++ variables/create-servers.sh
@@ -6,7 +6,8 @@ N="\e[0m"
 
 SERVER=$@
 INSTANCE_TYPE=
-SECURITY_GRUOP_ID=
+SECURITY_GRUOP_ID="sg-0f727fc59d89f7325"
+IMAGE_ID="ami-0f88e80871fd81e91"
 
 
 for i in $SERVER
@@ -21,4 +22,16 @@ fi
 
 echo -e  " Server name: $G $i $N "
 echo -e " Instance type: $G $INSTANCE_TYPE $N "
+
+echo " creating instance : $i "
+
+IP_ADDRESS=$( aws ec2 run-instances \
+  --image-id $IMAGE_ID \
+  --instance-type $INSTANCE_TYPE \
+  --key-name  awsdevops\
+  --security-group-ids $SECURITY_GROUP_ID \
+  --count 1 \
+  --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value="$i"}]' | jq -r '.Instances[0].PrivateIpAddress' )
+echo " Ip address of $i : $IP_ADDRESS "
+
   done
