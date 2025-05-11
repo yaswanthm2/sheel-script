80764917bb36c59e76cb44f1a7659e4c3adf8c3d
Yaswanth <yaswanth@>
2025-05-11 15:44:43 +0530

    new script for ec2 server creation

--- ec2-creation.sh
+++ ec2-creation.sh
@@ -1,33 +1,38 @@
 #!/bin/bash
 
-R="\e[31m"    #it will give text red color 
-G="\e[32m"    #it will give text green color 
-N="\e[0m"     #it will give normal color to text 
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
-IMAGE_ID=
+SERVER=$@
 INSTANCE_TYPE=
-SECURITY_GROUP_IP=
+SECURITY_GRUOP_ID="sg-0f727fc59d89f7325"
+IMAGE_ID="ami-0f88e80871fd81e91"
 
-DATE=$(date +%F)
-SCRIPTFILE_NAME=$0
-LOG_FILE=/tmp/$SCRIPTFILE_NAME-$DATE.log
-INSTANCE_TYPE=""
 
-INSATNCES_NAMES=("mongodb" "cart" "app" "mysql")
+for i in $SERVER
+  do 
 
-for i in ${INSATNCES_NAMES[@]}
-   do 
-      if [[ $i == mongodb || $i ==mysql ]];
-        then 
-          INSTACE_TYPE="t3.micro"
-        else
-          INSTANCE_TYPE="t2.micro"
-      fi 
-    echo " creating instace $i "
+if [[ $i == "mongodb" || $i == "mysql" ]]
+  then 
+     INSTANCE_TYPE=t3.micro
+  else
+     INSTANCE_TYPE=t2.micro
+fi
 
-    IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-groups-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jg -r '.Instances[0].PrivateIpAddress')  
+echo -e  " Server name: $G $i $N "
+echo -e " Instance type: $G $INSTANCE_TYPE $N "
 
-    echo " created $i instance IP : $IP_ADDRESS "
+echo " creating instance : $i "
 
-    done
+IP_ADDRESS=$( aws ec2 run-instances \
+  --image-id $IMAGE_ID \
+  --instance-type $INSTANCE_TYPE \
+  --key-name  awsdevops\
+  --security-group-ids $SECURITY_GROUP_ID \
+  --count 1 \
+  --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value='$i'}]' | jq -r '.Instances[0].PrivateIpAddress' )
+echo " Ip address of $i : $IP_ADDRESS "
+
+  done
 
